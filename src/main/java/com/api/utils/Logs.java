d3568ea48cdd70f7de61b5b2262e0993bdbe2965
bmvinayk <bmvinayk@gmail.com>
2020-08-01 10:52:24 +0530

    added logj4 and extent report

--- src/main/java/com/api/utils/Logs.java
+++ src/main/java/com/api/utils/Logs.java
@@ -1,11 +1,15 @@
 package com.api.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Reporter;
 
 public class Logs {
+	private static final Logger LOG = LogManager.getLogger(Logs.class.getName());
 	
 	public static void info(String message) {
 		Reporter.log(message, true);
+		LOG.info(message);
 	}
 
 }
--- src/test/java/com/api/tests/TestBase.java
+++ src/test/java/com/api/tests/TestBase.java
@@ -2,21 +2,29 @@ package com.api.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.io.File;
 import java.lang.reflect.Method;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.xml.XmlTest;
 
 import com.api.utils.Logs;
 import com.api.utils.RestExecutor;
 import com.api.utils.RestValidator;
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerAdapter;
 import com.github.javafaker.Faker;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+//	mvn clean test && mvn surefire-report:report-only && mvn site -DgenerateReports=false
+@Listeners(ExtentITestListenerAdapter.class)
 public class TestBase {
 	RequestSpecification request;
 	RestValidator restValidator;
@@ -24,6 +32,17 @@ public class TestBase {
 	Response response;
 	Faker fake;
 
+	@BeforeTest
+	public void logSetup(XmlTest test) {
+		String filePath = "logs";
+		File logFile = new File(filePath);
+		if(!logFile.exists()) {
+			logFile.mkdir();
+		}
+		
+		ThreadContext.put("ROUTINGKEY", filePath+File.separator+test.getName());
+	}
+	
 	@BeforeMethod
 	public void setup(Method method) {
 		request = given()
@@ -32,15 +51,16 @@ public class TestBase {
 					.accept(ContentType.JSON);
 		restExecutor = new RestExecutor(request);
 		restValidator = new RestValidator();
+		
 		logDetails(method.getName(), "started");
-		fake = new Faker();
+		fake = new Faker();		
 	}
 	
 	@AfterMethod
 	public void teardown(ITestResult result) {
 		String status = "PASSED";
 		if(result.getStatus()==ITestResult.FAILURE) {
-			status = "FAILED";
+			status = "FAILED";			
 		}
 		logDetails(result.getName(), status);
 	}
@@ -51,3 +71,4 @@ public class TestBase {
 		Logs.info("===============================================================================");
 	}
 }
+//	mvn clean test && mvn surefire-report:report-only && mvn site -DgenerateReports=false
